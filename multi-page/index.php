66c6cda0e0d02452e1b742a9a1530a514783236d
MrBogdan007 <58216389+MrBogdan007@users.noreply.github.com>
2020-02-17 12:02:25 +0200

    Nitrix

---
+++ multi-page/index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <title>Portfolio</title>
+    <!-- Custom styles for this template -->
+
+
+
+   <link href="css/style.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+  </head>
+
+  <body>
+
+ <?php include('nav.php'); ?>
+
+ 
+<div class="container">
+      <div class="starter-template">
+        <h1>Portfolio page</h1>
+        <p class="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae eaque, temporibus perferendis cum nesciunt neque voluptatibus, reprehenderit laboriosam consequuntur illo quisquam esse rerum vitae, illum tempora. Sapiente odit a nisi.</p>
+      </div>
+</div>
+   
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
---
+++ multi-page/nav.php
@@ -0,0 +1,12 @@
+    <nav class="navbar">
+      <div class="container">
+          <ul class="navbar-wrap">
+          <img src="img/0.png" alt="Nitrix" class="logo blog__logo">
+          <img src="img/circle.png" class="dot"alt="Dot">
+            <li><a href="index.php">Portfolio</a></li>
+            <li><a href="career.php">Career</a></li>
+            <li class="active"><a href="blog.php">Blog</a></li>
+            <li><a href="contact.php">Contact</a></li>
+          </ul>
+      </div>
+    </nav>
